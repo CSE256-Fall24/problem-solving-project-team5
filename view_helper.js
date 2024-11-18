f157e44408bad9cfaf70d9d99b8a2789ab7c6b8c
emoseswustl <e.g.moses@wustl.edu>
2024-11-17 19:09:36 -0600

    adding click listeners for perm_inheritance and perm_replace_child_permissions checkboxes

--- view_helper.js
+++ view_helper.js
@@ -201,6 +201,8 @@ function define_new_effective_permissions(id_prefix, add_info_col = false, which
     define_attribute_observer(effective_container, 'username', update_effective_contents)
     define_attribute_observer(effective_container, 'filepath', update_effective_contents)
     $('.groupcheckbox').change(update_effective_contents)
+    $('#adv_perm_inheritance').change(update_effective_contents)
+    $('#adv_perm_replace_child_permissions').change(update_effective_contents)
     return effective_container
 }
 
